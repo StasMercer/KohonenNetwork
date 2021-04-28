8b9f87bb0df163344521a353677dff0df88e2400
Stas <pro100.stas.ru@gmail.com>
2021-04-28 15:27:48 +0300

    self added

--- kohonen.py
+++ kohonen.py
@@ -2,16 +2,16 @@ import math
 
 
 class Kohonen:
-    def __init__(start_radius, start_learning_rate, time_constant):
+    def __init__(self, start_radius, start_learning_rate, time_constant):
         self.start_radius = start_radius
         self.start_learning_rate = start_learning_rate
         self.time_constant = time_constant
 
-    def get_radius(iteration_number):
+    def get_radius(self, iteration_number):
         return self.start_radius * math.exp(-iteration_number / self.time_constant)
 
-    def get_learning_rate(iteration_number):
+    def get_learning_rate(self, iteration_number):
         return self.start_learning_rate * math.exp(-iteration_number / self.time_constant)
         
-    def neibourghood_function(iteration_number):
-        return math.exp(-self.distance ** 2 / (2 * get_radius(iteration_number)))
+    def neibourghood_function(self, iteration_number):
+        return math.exp(-self.distance ** 2 / (2 * self.get_radius(iteration_number)))

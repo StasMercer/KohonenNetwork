a11f19ed5682d3019ca4767b617d3fcc4d7f92f9
selposhny <83341421+selposhny@users.noreply.github.com>
2021-04-28 18:54:05 +0300

    searching BMU in the list of nodes

--- main.py
+++ main.py
@@ -9,15 +9,32 @@ class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.weights = np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
+        self.weights = get_random_color()
 
     def get_color(self):
         return (self.weights[0], self.weights[1], self.weights[2])
+
+def get_random_color():
+    return np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
 
 
 def train(cords):
     print(cords)
     print('train printed')
+    
+    for i in range(10): # n attempts
+        input_vector = get_random_color()
+
+        min_distance = math.sqrt(3) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
+        best_matching_unit = None
+
+        for node in cords:
+            euclid_distance = np.linalg.norm(input_vector - node.weights)
+            if euclid_distance < min_distance:
+                min_distance = euclid_distance
+                best_matching_unit = node
+
+        print("")
 
 
 def onclick(event):
@@ -29,7 +46,6 @@ def onclick(event):
     if event.inaxes == ax:
         node = Node(event.xdata, event.ydata)
         cords.append(node)
-        cords.append([event.xdata, event.ydata])
         ax.plot(event.xdata, event.ydata, 'o', color=node.get_color())
         ax.figure.canvas.draw()
         

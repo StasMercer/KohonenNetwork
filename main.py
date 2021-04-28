64278bf6388a8e6d2e12ecb667d136c88fd0bafc
selposhny <83341421+selposhny@users.noreply.github.com>
2021-04-28 20:50:59 +0300

    it's working better, but i missed something...

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ import numpy as np
 import random
 from matplotlib.widgets import Button
 
+TIME = 2
+WIDTH = 1
+HEIGHT = 1
 
 class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.color = np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
+        self.weights = get_random_weights()
 
     def get_rgb(self):
-        return (self.color[0], self.color[1], self.color[2])
+        return (self.weights[0], self.weights[1], self.weights[2])
 
-    def get_weights(self):
-        return self.x, self.y, self.color[0], self.color[1], self.color[2]
-
-    def update_weights(self, a, b, input):
-        ax.plot(self.x, self.y, 'o', color=(1, 1, 1))
-        ax.figure.canvas.draw()
-        self.x += a * b * (input[0] - self.get_weights()[0])
-        self.y += a * b * (input[0] - self.get_weights()[1])
-        self.color[0] += a * b * (input[2] - self.get_weights()[2])
-        self.color[1] += a * b * (input[3] - self.get_weights()[3])
-        self.color[2] += a * b * (input[4] - self.get_weights()[4])
+    def update_weights(self, a, b, input_vector):
+        self.weights[0] += a * b * (input_vector[0] - self.weights[0])
+        self.weights[1] += a * b * (input_vector[1] - self.weights[1])
+        self.weights[2] += a * b * (input_vector[2] - self.weights[2])
         ax.plot(self.x, self.y, 'o', color=self.get_rgb())
         ax.figure.canvas.draw()
 
@@ -42,33 +38,42 @@ class Kohonen:
         return self.start_learning_rate * math.exp(-iteration_number / self.time_constant)
         
     def neibourghood_function(self, iteration_number, distance):
-        return math.exp(-distance ** 2 / (2 * self.get_radius(iteration_number)))
+        return math.exp(-1 * (distance ** 2) / (2 * self.get_radius(iteration_number)))
         
     def train(self, nodes):
         print(nodes)
         print('train printed')
-        for _ in range(10):
-            for iteration_number in range(3):
-                input_vector = np.array([random.uniform(0, 1) for _ in range(5)])
 
-                best_matching_unit, min_distance = self.get_best_matching_unit(input_vector, nodes)
+        n_input_vectors = 2
+        input_vectors = np.array([get_random_weights() for _ in range(n_input_vectors)])
 
-                a = self.get_learning_rate(iteration_number)
-                b = self.neibourghood_function(iteration_number, min_distance)
+        for iteration_number in range(TIME):
+            for input_vector in input_vectors:
+                best_matching_unit = self.get_best_matching_unit(input_vector, nodes)
 
-                best_matching_unit.update_weights(a, b, input_vector)
+                learning_rate = self.get_learning_rate(iteration_number)
+                
+                for node in nodes:
+                    if node != best_matching_unit: # ?? is it in need
+                        distance = np.linalg.norm(best_matching_unit.weights - node.weights)
+                        neibourghood = self.neibourghood_function(iteration_number, distance)
+                        node.update_weights(learning_rate, neibourghood, input_vector)
 
     def get_best_matching_unit(self, input_vector, nodes):
-        min_distance = math.sqrt(5) # n features
+        min_distance = math.sqrt(3) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
         best_matching_unit = None
 
         for node in nodes:
-            euclid_distance = np.linalg.norm(input_vector - node.get_weights())
+            euclid_distance = np.linalg.norm(input_vector - node.weights)
             if euclid_distance < min_distance:
                 min_distance = euclid_distance
                 best_matching_unit = node
 
-        return best_matching_unit, min_distance
+        return best_matching_unit
+
+
+def get_random_weights():
+    return np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
 
 
 def onclick(event):
@@ -84,22 +89,24 @@ def onclick(event):
         ax.figure.canvas.draw()
         
     if event.inaxes == btn_train_axes:
-        model = Kohonen(10, 1, 1)
+        start_radius = max(WIDTH, HEIGHT) / 4
+        time_constant = TIME / math.log(start_radius)
+        model = Kohonen(start_radius, 0.8, time_constant) # set parameters not in this function
         model.train(nodes)
     
     if event.inaxes == btn_reset_axes:
         nodes.clear()
         ax.cla()
-        ax.set_xlim([0, 1])
-        ax.set_ylim([0, 1])
+        ax.set_xlim([0, WIDTH])
+        ax.set_ylim([0, HEIGHT])
 
 
 if __name__ == '__main__':
     plt.subplots_adjust(bottom=0.2)
     ax = plt.subplot()
 
-    ax.set_xlim([0, 1])
-    ax.set_ylim([0, 1])
+    ax.set_xlim([0, WIDTH])
+    ax.set_ylim([0, HEIGHT])
 
     nodes = []
 

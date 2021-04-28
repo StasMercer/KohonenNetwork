48ec82c3c13e32dee017bea735321725440ed54d
selposhny <83341421+selposhny@users.noreply.github.com>
2021-04-28 20:02:47 +0300

    something is working...

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.color = get_random_weights()
+        self.color = np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
 
     def get_rgb(self):
         return (self.color[0], self.color[1], self.color[2])
@@ -17,10 +17,14 @@ class Node:
     def get_weights(self):
         return self.x, self.y, self.color[0], self.color[1], self.color[2]
 
-    def update_weights(self, a, b, x):
-        self.color[0] += a * b * (x[0] - self.color[0])
-        self.color[1] += a * b * (x[1] - self.color[1])
-        self.color[2] += a * b * (x[2] - self.color[2])
+    def update_weights(self, a, b, input):
+        ax.plot(self.x, self.y, 'o', color=(1, 1, 1))
+        ax.figure.canvas.draw()
+        self.x += a * b * (input[0] - self.get_weights()[0])
+        self.y += a * b * (input[0] - self.get_weights()[1])
+        self.color[0] += a * b * (input[2] - self.get_weights()[2])
+        self.color[1] += a * b * (input[3] - self.get_weights()[3])
+        self.color[2] += a * b * (input[4] - self.get_weights()[4])
         ax.plot(self.x, self.y, 'o', color=self.get_rgb())
         ax.figure.canvas.draw()
 
@@ -43,32 +47,28 @@ class Kohonen:
     def train(self, nodes):
         print(nodes)
         print('train printed')
+        for _ in range(10):
+            for iteration_number in range(3):
+                input_vector = np.array([random.uniform(0, 1) for _ in range(5)])
 
-        for iteration_number in range(100):
-            input_vector = get_random_weights()
+                best_matching_unit, min_distance = self.get_best_matching_unit(input_vector, nodes)
 
-            best_matching_unit, min_distance = self.get_best_matching_unit(input_vector, nodes)
+                a = self.get_learning_rate(iteration_number)
+                b = self.neibourghood_function(iteration_number, min_distance)
 
-            a = self.get_learning_rate(iteration_number)
-            b = self.neibourghood_function(iteration_number, min_distance)
-
-            best_matching_unit.update_weights(a, b, input_vector)
+                best_matching_unit.update_weights(a, b, input_vector)
 
     def get_best_matching_unit(self, input_vector, nodes):
-        min_distance = math.sqrt(3) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
+        min_distance = math.sqrt(5) # n features
         best_matching_unit = None
 
         for node in nodes:
-            euclid_distance = np.linalg.norm(input_vector - node.color)
+            euclid_distance = np.linalg.norm(input_vector - node.get_weights())
             if euclid_distance < min_distance:
                 min_distance = euclid_distance
                 best_matching_unit = node
 
         return best_matching_unit, min_distance
-
-
-def get_random_weights():
-    return np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
 
 
 def onclick(event):
@@ -90,16 +90,16 @@ def onclick(event):
     if event.inaxes == btn_reset_axes:
         nodes.clear()
         ax.cla()
-        ax.set_xlim([0, 10])
-        ax.set_ylim([0, 10])
+        ax.set_xlim([0, 1])
+        ax.set_ylim([0, 1])
 
 
 if __name__ == '__main__':
     plt.subplots_adjust(bottom=0.2)
     ax = plt.subplot()
 
-    ax.set_xlim([0, 10])
-    ax.set_ylim([0, 10])
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1])
 
     nodes = []
 

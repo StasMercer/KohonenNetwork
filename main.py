c39223b4dc86d990cd25dd25b4935e57d2012b21
selposhny <83341421+selposhny@users.noreply.github.com>
2021-04-28 22:52:28 +0300

    colors to cords

--- main.py
+++ main.py
@@ -5,26 +5,35 @@ import random
 from matplotlib.widgets import Button
 
 TIME = 1
-WIDTH = 10
-HEIGHT = 10
+WIDTH = 1
+HEIGHT = 1
 
 class Node:
     def __init__(self, cords):
         self.cords = np.array(cords)
-        self.weights = get_random_weights()
+        # self.weights = get_random_weights()
 
     def get_rgb(self):
         return (self.weights[0], self.weights[1], self.weights[2])
 
     def update_weights(self, a, b, input_vector):
-        for i in range(3):
-            self.weights[i] += a * b * (input_vector[i] - self.weights[i])
-            if self.weights[i] > 1:
-                self.weights[i] = 1
-            elif self.weights[i] < 0:
-                self.weights[i] = 0
+        ax.plot(self.cords[0], self.cords[1], 'o', color='white')
+        ax.figure.canvas.draw()
+        for i in range(2):
+        # for i in range(3):
+            self.cords[i] += a * b * (input_vector[i] - self.cords[i])
+            # self.weights[i] += a * b * (input_vector[i] - self.weights[i])
+            # if self.weights[i] > 1:
+            #     self.weights[i] = 1
+            # elif self.weights[i] < 0:
+            #     self.weights[i] = 0
+            if self.cords[i] > 1:
+                self.cords[i] = 1
+            elif self.cords[i] < 0:
+                self.cords[i] = 0
 
-        ax.plot(self.cords[0], self.cords[1], 'o', color=self.get_rgb())
+        ax.plot(self.cords[0], self.cords[1], 'ok')
+        # ax.plot(self.cords[0], self.cords[1], 'o', color=self.get_rgb())
         ax.figure.canvas.draw()
 
 
@@ -47,11 +56,12 @@ class Kohonen:
         print(nodes)
         print('train printed')
 
-        n_input_vectors = 3
-        input_vectors = np.array([get_random_weights() for _ in range(n_input_vectors)])
+        # n_input_vectors = 3
+        # input_vectors = np.array([get_random_weights() for _ in range(n_input_vectors)])
+        input_vectors = np.array([(0, 0), (1, 0), (0, 1), (1, 1)])
 
-        for input_vector in input_vectors:
-            for iteration_number in range(TIME):
+        for iteration_number in range(TIME):
+            for input_vector in input_vectors:
                 best_matching_unit = self.get_best_matching_unit(input_vector, nodes)
 
                 learning_rate = self.get_learning_rate(iteration_number)
@@ -59,15 +69,18 @@ class Kohonen:
                 for node in nodes:
                     # if node != best_matching_unit: # ?? is it in need
                     distance = np.linalg.norm(best_matching_unit.cords - node.cords)
-                    neibourghood = self.neibourghood_function(iteration_number, distance)
-                    node.update_weights(learning_rate, neibourghood, input_vector)
+                    if distance != 0:
+                        neibourghood = self.neibourghood_function(iteration_number, distance)
+                        node.update_weights(learning_rate, neibourghood, input_vector)
 
     def get_best_matching_unit(self, input_vector, nodes):
-        min_distance = math.sqrt(3) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
+        min_distance = math.sqrt(2) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
+        # min_distance = math.sqrt(3) # sqrt((0 - 1) ** 2 + (0 - 1) ** 2 + (0 - 1) ** 2)
         best_matching_unit = None
 
         for node in nodes:
-            euclid_distance = np.linalg.norm(input_vector - node.weights)
+            euclid_distance = np.linalg.norm(input_vector - node.cords)
+            # euclid_distance = np.linalg.norm(input_vector - node.weights)
             if euclid_distance < min_distance:
                 min_distance = euclid_distance
                 best_matching_unit = node
@@ -75,8 +88,9 @@ class Kohonen:
         return best_matching_unit
 
 
-def get_random_weights():
-    return np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
+# def get_random_weights():
+#     return np.array([random.uniform(0, 1), random.uniform(0, 1)])
+#     # return np.array([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
 
 
 def onclick(event):
@@ -88,11 +102,12 @@ def onclick(event):
     if event.inaxes == ax:
         node = Node((event.xdata, event.ydata))
         nodes.append(node)
-        ax.plot(event.xdata, event.ydata, 'o', color=node.get_rgb())
+        ax.plot(event.xdata, event.ydata, 'ok')
+        # ax.plot(event.xdata, event.ydata, 'o', color=node.get_rgb())
         ax.figure.canvas.draw()
         
     if event.inaxes == btn_train_axes:
-        start_radius = max(HEIGHT, WIDTH) / 8
+        start_radius = max(HEIGHT, WIDTH) / 16
         time_constant = TIME / math.log(start_radius)
         model = Kohonen(start_radius, 0.8, time_constant) # set parameters not in this function
         model.train(nodes)
